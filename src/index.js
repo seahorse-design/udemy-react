9b39dc359fc3d29f0ecc75e0ff9ff883b9225708
saki matsushita <saki.seahorse@gmail.com>
2022-08-14 05:12:52 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import "./styles.css";
+// import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+const trainer = {
+  name: "satoshi",
+  age: 10,
+};
+trainer.name = "shigeru";
+console.log(trainer);
+
